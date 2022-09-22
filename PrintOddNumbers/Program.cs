708292b0840012025c024cd564f60563fd898aa1
YoginiMac12 <113415806+YoginiMac12@users.noreply.github.com>
2022-09-22 22:41:42 +0530

    Add files via upload

---
+++ PrintOddNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int OddCount = 0;
+            Console.WriteLine("50 to 80 Odd Number:");
+            for (int i = 50; i <= 80; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    OddCount++;
+                }
+            }
+            Console.WriteLine("Total Odd Number Count=" + OddCount);
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
